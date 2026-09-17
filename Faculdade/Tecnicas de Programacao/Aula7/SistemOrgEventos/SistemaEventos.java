ff9153e06b8942c86ade8f3a7d391fa394d5fe6a
HenriLSchons-Dev <hlopesschons@gmail.com>
2026-09-17 16:09:52 -0300

    alteracao final, atividade extra aula 7

--- Faculdade/Tecnicas de Programacao/Aula7/SistemOrgEventos/SistemaEventos.java
+++ Faculdade/Tecnicas de Programacao/Aula7/SistemOrgEventos/SistemaEventos.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
 public class SistemaEventos {
     public static void main(String[] args) {
+
         try {
             Espaco meuEspaco = new Espaco("Villa del Balbianello", "ás margens do Lago de Como, em Lenno - Italia", 200, "Festa");
+
             Evento meuEvento = new Evento("Star Wars Encounter", "11/09", 20000, 75.99);
+
             Organizador meuOrganizador = new Organizador("Henri", "109102992", "Festas");
+
+            List<Evento> eventos = new ArrayList<>();
+
+            eventos.add(meuEvento);
 
             meuOrganizador.assumirEvento(meuEvento, meuEspaco);
             meuEvento.verificarCapacidade(meuEspaco);
+
+            System.out.println("\nEventos cadastrados:");
+
+            for (Evento evento : eventos) {
+                evento.exibirDados();
+            }
 
         } catch (IllegalArgumentException e) {
             System.out.println("Erro: " + e.getMessage());
